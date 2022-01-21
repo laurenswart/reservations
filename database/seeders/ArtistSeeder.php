01fb623e087068339640e008a1619389079bbca0
lswart <lauren.swart@gmail.com>
2022-01-21 20:39:50 +0100

    Artist: model, migration, seeder

---
+++ database/seeders/ArtistSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Models\Artist;
+
+class ArtistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Empty the table first
+        Artist::truncate();
+
+        //Define data
+        $artists = [
+            ['firstname'=>'Daniel','lastname'=>'Marcelin'],
+            ['firstname'=>'Philippe','lastname'=>'Laurent'],
+            ['firstname'=>'Marius','lastname'=>'Von Mayenburg'],
+            ['firstname'=>'Olivier','lastname'=>'Boudon'],
+            ['firstname'=>'Anne Marie','lastname'=>'Loop'],
+            ['firstname'=>'Pietro','lastname'=>'Varasso'],
+            ['firstname'=>'Laurent','lastname'=>'Caron'],
+            ['firstname'=>'Élena','lastname'=>'Perez'],
+            ['firstname'=>'Guillaume','lastname'=>'Alexandre'],
+            ['firstname'=>'Claude','lastname'=>'Semal'],
+            ['firstname'=>'Laurence','lastname'=>'Warin'],
+            ['firstname'=>'Pierre','lastname'=>'Wayburn'],
+            ['firstname'=>'Gwendoline','lastname'=>'Gauthier'],
+        ];
+        
+        //Insert data in the table
+        DB::table('artists')->insert($artists);
+        
+    }
+}
