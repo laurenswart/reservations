df92ac1517de132aea11ce33ac50b7476ace1fb8
lswart <lauren.swart@gmail.com>
2022-05-06 12:33:52 +0200

    Bugs d'affichage réglés

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -11,19 +11,21 @@
     </div>
 
 
-    <ul>
+    <div class="row my-4">
     @foreach($locations as $location)
-        <div class="media-object">
+        <div class="media-object col-6 col-md-4 my-4">
             <div class="media-object-section">
             <a href="{{ $location->website }}"><i class="fa-solid fa-link"></i></a>
             </div>
             <div class="media-object-section">
                 <h5><a href="{{ $location->website }}">{{ $location->designation }}</a></h5>
                 <p>{{ $location->address }}<br>{{ $location->locality->postal_code }} {{ $location->locality->locality }}</p>
+                <button onclick="afficher({{ $location->id }})" class="mapButton"><i class="fas fa-map-marker-alt pl-0"></i>Afficher sur la carte</button>
             </div>
+           
         </div>
     @endforeach
-    </ul>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.14.1/build/ol.js"></script>
@@ -32,6 +34,12 @@
       var attribution = new ol.control.Attribution({
      collapsible: false
  });
+
+ let view = new ol.View({
+         center: ol.proj.fromLonLat([4.379429, 50.824997]),
+         maxZoom: 18,
+         zoom: 13
+     })
 
  var map = new ol.Map({
      controls: ol.control.defaults({attribution: false}).extend([attribution]),
@@ -45,18 +53,14 @@
          })
      ],
      target: 'map',
-     view: new ol.View({
-         center: ol.proj.fromLonLat([4.379429, 50.824997]),
-         maxZoom: 18,
-         zoom: 13
-     })
+     view: view
  });
 
  var layer = new ol.layer.Vector({
      source: new ol.source.Vector({
          features: [
              <?php foreach($locations as $location){ ?>
-                <?= "new ol.Feature({geometry: new ol.geom.Point(ol.proj.fromLonLat([".$location->longitude." , ".$location->latitude."])), name: '".addslashes($location->designation)."', address:'".addslashes($location->address).'<br>'.$location->locality->postal_code.' '.$location->locality->locality."', website:'".$location->website."'}),"?>
+                <?= "new ol.Feature({geometry: new ol.geom.Point(ol.proj.fromLonLat([".$location->longitude." , ".$location->latitude."])), name: '".addslashes($location->designation)."', address:'".addslashes($location->address).'<br>'.$location->locality->postal_code.' '.$location->locality->locality."', website:'".$location->website."', id:".$location->id."}),"?>
              <?php } ?>
          ]
      }),
@@ -72,13 +76,24 @@ style = new ol.style.Style({
     text: new ol.style.Text({
         text: feature.get('name'),
         font: '20px Calibri,sans-serif',
-        fill: new ol.style.Fill({ color: '#000' }),
-        stroke: new ol.style.Stroke({
-            color: '#fff', width: 2
-        })
-    })
-});
-feature.setStyle(style);
+        fill: new ol.style.Fill({ color: '#fff' }),
+        
+        offsetY:-25,
+        backgroundFill:new ol.style.Fill({ color: '#000' }),
+        padding:[3,5 ,1 ,5 ]  
+        
+    }),
+    
+    image: new ol.style.Circle({
+            fill: new ol.style.Fill({color: '#000'}),
+            stroke: new ol.style.Stroke({
+                    color: '#d7a32d',
+                    width: 1.25,
+                    }),
+            radius: 5,
+        }),
+    });
+    feature.setStyle(style);
 });
  map.addLayer(layer);
  
@@ -95,12 +110,12 @@ feature.setStyle(style);
     });
     map.addOverlay(overlay);
 
-    closer.onclick = function() {
+closer.onclick = function() {
         overlay.setPosition(undefined);
         closer.blur();
         return false;
     };
-     map.on('singleclick', function (event) {
+map.on('pointermove', function (event) {
      if (map.hasFeatureAtPixel(event.pixel) === true) {
          var coordinate = event.coordinate;
          var feature = map.forEachFeatureAtPixel(event.pixel,
@@ -116,6 +131,18 @@ feature.setStyle(style);
          closer.blur();
      }
  });
+
+ function afficher(locationId){
+    //find feature with id = locationId
+    layer.getSource().forEachFeature(function(feature){
+        if(feature.get('id')==locationId){
+            //found the feature
+            let point = feature.getGeometry();
+            view.fit(point, { minResolution: 5, duration:500});
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+        }
+        });
+ }
 
 </script>
 
