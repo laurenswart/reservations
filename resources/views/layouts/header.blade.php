be92129d53e32c265326966c3e963c52bca3e04f
lswart <lauren.swart@gmail.com>
2022-06-08 10:32:53 +0200

    favicon

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>Left Sidebar - Helios by HTML5 UP</title>
+		<title>The Case Of Shows</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
@@ -14,7 +14,7 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
 		<link rel="stylesheet" href="{{ asset('css/imageFlex.css') }}" />
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
-		
+		<link rel="shortcut icon" href="{{ asset('favicon.png') }}">
 		<noscript><link rel="stylesheet" href="{{ asset('css/noscript.css') }}" /></noscript>
 	</head>
 	<body class="left-sidebar is-preload">
